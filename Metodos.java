849a856e8b8eca3d83962c2f73429e591e719b62
jugato <julianricardo_21@hotmail.com>
2025-02-12 16:17:54 -0500

    rama develop para los desarrollos

---
+++ Metodos.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Metodos 
+{
+    public int SolicitarDimension()
+    {
+        Scanner sc = new Scanner(System.in);
+        int d = 0;
+        System.out.println("Ingrese la dimension de la matriz: ");
+        d = sc.nextInt();
+        return d;
+    }
+
+    public int [][] LlenarMatriz( int d )
+    {
+        int [][] m = new int[d][d];
+        for (int i = 0; i < m.length; i++)
+        {
+            for (int j = 0; j < m.length; j++)
+            {
+                m[i][j] = (int)(Math.random() * 50 + 1); 
+            }
+        }
+        return m;
+    }
+}
