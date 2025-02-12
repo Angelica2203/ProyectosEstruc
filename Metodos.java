d20b44cf34f03307b67f84e6690d0863f113cc7a
jugato <julianricardo_21@hotmail.com>
2025-02-12 16:43:48 -0500

    metodo mostrar matriz original

--- Metodos.java
+++ Metodos.java
@@ -23,4 +23,17 @@ public class Metodos
         }
         return m;
     }
+    public void MostrarMatriz( int [][] m )
+    {
+        for (int i = 0; i < m.length; i++)
+        {
+            for (int j = 0; j < m.length; j++)
+            {
+                System.out.print(" - " + m[i][j]);
+
+            }
+            System.out.println();
+        }
+        
+    }
 }
--- Principal.java
+++ Principal.java
@@ -2,7 +2,10 @@ public class Principal
 {
     public static void main(String[] args) 
     {
-        
+        Metodos Mt = new Metodos();
+        int d = Mt.SolicitarDimension();;
+        int [][] m = Mt.LlenarMatriz(d);
+        Mt.MostrarMatriz(m);
     }
     
 }
